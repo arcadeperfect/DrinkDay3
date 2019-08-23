dae54147736f4a673ef5b2a65b7fdd441c23e3e1
arcadeperfect <alexander.harding@gmail.com>
2019-08-23 14:48:11 +0100

    will this work?

---
+++ dd_matrix_display.py
@@ -0,0 +1,16 @@
+import rgbmatrix
+
+
+class display(object):
+
+	def __init__(self, drinkImage, mult):
+		self.drinkImage = drinkImage
+		self.mult=mult
+		self.image = self.load_image()
+		print('matrix display')
+
+	def update():
+		print("updating display with", image)
+
+	def load_image():
+		
