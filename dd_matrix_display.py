013aa489de35950de8355544227ab92fa58155a5
arcadeperfect <alexander.harding@gmail.com>
2019-08-23 15:28:50 +0100

    now running on pi

--- dd_matrix_display.py
+++ dd_matrix_display.py
@@ -6,20 +6,19 @@ from dd_util import get_img_path
 class display(object):
 
 	def __init__(self, drinkImage, mult):
+		options = RGBMatrixOptions()
+		options.rows = 32
+		options.chain_length = 1
+		options.parallel = 1
+		options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+		self.matrix = RGBMatrix(options=options)
 		self.drinkImage = drinkImage
 		self.mult=mult
 		self.image_location = get_img_path()
 		self.PILImage = self.load_image(os.path.join(self.image_location, drinkImage.file_name))
 		print('matrix display')
 
-		options = RGBMatrixOptions()
-		options.rows = 32
-		options.chain_length = 1
-		options.parallel = 1
-		options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
 
-		print('creating matrix')
-		self.matrix = RGBMatrix(options=options)
 
 	def update(self):
 		print("updating display with", self.PILImage)
@@ -36,7 +35,8 @@ class display(object):
 
 	def show(self, PILImage):
 		PILImage.thumbnail((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
+		print('show')
 
-		#self.matrix.SetImage(PILImage.convert('RGB'))
+		self.matrix.SetImage(PILImage.convert('RGB'))
 
 
